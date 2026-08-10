3d68bbcb02a589551a8571ad64af94408e9cc73e
apeirodentro-sys <apeirodentro@gmail.com>
2026-08-10 14:34:00 -0300

    Tried implementing UDP, learned more about it... - Implemented UDP_Connect_Port. - Implemented UDP_Port_Scanner.

--- Port Scanner/main.py
+++ Port Scanner/main.py
@@ -7,7 +7,6 @@ def TCP_connect_port(ip, port):
     client.close()
     return value
 
-
 def TCP_port_scanner(ip, ports):
     open_ports = []
     closed_ports = []
@@ -24,6 +23,30 @@ def TCP_port_scanner(ip, ports):
             blocked_ports.append(port)
     return [open_ports,closed_ports,blocked_ports]
 
+def UDP_connect_port(ip, port):
+    MAX_COUNT = 2
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.settimeout(1)
+    count = 0
+    while count <= MAX_COUNT:
+        count+=1
+        client.sendto(b"PING", (ip, port))
+        data, _ = client.recvfrom(1024)
+        if data.decode('utf-8').strip() == "PONG":
+            client.close()
+            return 1
+    return 0
+
+def UDP_port_scanner(ip, ports): #assuming there is a response mechanism on the server
+    open_ports = []
+    notopen_ports = []
+    for port in ports:
+        if UDP_connect_port(ip, port) == 1:
+            open_ports.append(port)
+        else:
+            notopen_ports.append(port)
+        
+    
 TCP_port_scanner('127.0.0.1', [9000,1])
 
 
